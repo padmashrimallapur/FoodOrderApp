c75e34a21885e0e2dd5fef4f9f91d181c818dae8
padmashrimallapur <padmashri.mallapur@gmail.com>
2021-02-22 22:58:34 -0500

    Adding click Listener to recyclerview to open detailsActivity

--- app/src/main/java/com/example/foodorderapp/DetailActivity.java
+++ app/src/main/java/com/example/foodorderapp/DetailActivity.java
@@ -1,4 +1,30 @@
 package com.example.foodorderapp;
 
-public class DetailActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.foodorderapp.databinding.ActivityDetailBinding;
+
+public class DetailActivity extends AppCompatActivity {
+
+    ActivityDetailBinding binding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        int image = getIntent().getIntExtra("image", 0);
+        int price = Integer.parseInt(getIntent().getStringExtra("price"));
+        String foodName = getIntent().getStringExtra("foodName");
+        String description = getIntent().getStringExtra("description");
+
+        binding.detailImageId.setImageResource(image);
+        binding.pricelb.setText(String.format("%d", price));
+        binding.foodDeatilDescription.setText(description);
+        binding.foodNameId.setText(foodName);
+    }
 }
--- app/src/main/java/com/example/foodorderapp/adapters/MainAdapter.java
+++ app/src/main/java/com/example/foodorderapp/adapters/MainAdapter.java
@@ -1,6 +1,7 @@
 package com.example.foodorderapp.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.foodorderapp.DetailActivity;
 import com.example.foodorderapp.R;
 import com.example.foodorderapp.models.MainModel;
 
@@ -39,6 +41,19 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
         holder.foodName.setText(model.getFoodName());
         holder.price.setText(model.getPrice());
         holder.description.setText(model.getDescription());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, DetailActivity.class);
+                intent.putExtra("image", model.getImage());
+                intent.putExtra("price", model.getPrice());
+                intent.putExtra("description", model.getDescription());
+                intent.putExtra("foodName", model.getFoodName());
+                context.startActivity(intent);
+
+            }
+        });
     }
 
     @Override
